a612a3fc1292849ce58392960b550da75431be4f
shariful2011 <murad_mouri@yahoo.com>
2021-01-13 23:26:16 +0600

    auth 100% done

---
+++ lib/ui/pages/home/body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,2 +1,3 @@
 export 'auth/auth.dart';
+export 'home/home.dart';
 export 'onboarding/onboarding.dart';
